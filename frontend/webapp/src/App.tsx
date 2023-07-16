ef683182fe075d4343566c9a7aad5fb0931c299d
SachaCR <sacha@spendesk.com>
2023-07-16 14:18:48 +0200

    Add Auth0 login test

--- frontend/webapp/src/App.tsx
+++ frontend/webapp/src/App.tsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+  const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  };
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +37,15 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Auth0Provider
+          domain="dev-golang-test.eu.auth0.com"
+          clientId="KiigfCeTUPYc9SnQInAfN0RPkne2O3Wd"
+          authorizationParams={{
+            redirect_uri: window.location.origin
+          }}
+        >
+      <LoginButton></LoginButton>
+      </Auth0Provider>
     </>
   )
 }
